077854caba637b092a5d57588c358d4ca2fdcd2d
AnshuVankaraju <138388479+AnshuVankaraju@users.noreply.github.com>
2023-10-30 21:42:23 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,14 @@
+import random
+
+characters = ["A", 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+new_pass=eval(input("Enter the length of the password "))
+password = " "
+for char in range(1, new_pass + 1):
+    password = password+random.choice(characters)
+
+print("Your new password is ", password)
+        
+
+
+
